2038d1daabda13727664f22b357ffa86253682d2
mchoshi023 <mchoshi023@DESKTOP-Q9RGAC6>
2024-09-27 19:37:30 +0200

    Added landing page

--- skills-match/src/App.js
+++ skills-match/src/App.js
@@ -1,17 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RegistrationForm from './RegistrationForm'; // Ensure this import is present
 import Dashboard from './components/Dashboard'; // Adjust the path if necessary
+import Pulane from './pulane.jsx';
+
 
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path= "/" element={<Pulane/>}/>
         <Route path="/" element={<RegistrationForm />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );
 }
+
 
 export default App;
